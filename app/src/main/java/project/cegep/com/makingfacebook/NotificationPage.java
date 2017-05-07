3afa6ba7ded5a3963632793fc315d70cf4b775d8
sahilbhatoa93 <sahil.bhatoa93@gmail.com>
2017-05-07 15:09:01 -0400

    Added Notification Page for initial Commit

---
+++ app/src/main/java/project/cegep/com/makingfacebook/NotificationPage.java
@@ -0,0 +1,13 @@
+package project.cegep.com.makingfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_page);
+    }
+}
